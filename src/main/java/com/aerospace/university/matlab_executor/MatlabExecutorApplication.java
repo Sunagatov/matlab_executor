ff83abb416080cf1ab16cacd34f3d25b5caf94b8
Zufar_Sunagatov <zufar.sunagatov@gmail.com>
2020-04-19 21:06:21 +0400

    Initial commit

---
+++ src/main/java/com/aerospace/university/matlab_executor/MatlabExecutorApplication.java
@@ -0,0 +1,12 @@
+package com.aerospace.university.matlab_executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatlabExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatlabExecutorApplication.class, args);
+    }
+}
