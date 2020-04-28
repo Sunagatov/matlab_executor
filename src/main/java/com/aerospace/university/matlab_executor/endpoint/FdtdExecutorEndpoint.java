bd870fb013a354305b4149b0a82015ed58226168
Zufar_Sunagatov <zufar.sunagatov@gmail.com>
2020-04-28 19:22:19 +0400

    FdtdArguments for 1D, 2D, 3D  were added to Endpoint class.

--- src/main/java/com/aerospace/university/matlab_executor/endpoint/FdtdExecutorEndpoint.java
+++ src/main/java/com/aerospace/university/matlab_executor/endpoint/FdtdExecutorEndpoint.java
@@ -1,9 +1,13 @@
 package com.aerospace.university.matlab_executor.endpoint;
 
+import com.aerospace.university.matlab_executor.model.Fdtd1Darguments;
+import com.aerospace.university.matlab_executor.model.Fdtd2Darguments;
+import com.aerospace.university.matlab_executor.model.Fdtd3Darguments;
 import com.aerospace.university.matlab_executor.service.FdtdExecutorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,25 +17,25 @@ import java.io.IOException;
 @RequestMapping(value = "api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class FdtdExecutorEndpoint {
 
-    private final  FdtdExecutorService fdtdExecutorService;
+    private final FdtdExecutorService fdtdExecutorService;
 
     @Autowired
     public FdtdExecutorEndpoint(FdtdExecutorService fdtdExecutorService) {
         this.fdtdExecutorService = fdtdExecutorService;
     }
 
-    @GetMapping("/fdtd1D")
-    public void executeFDTD1D() throws IOException {
-        this.fdtdExecutorService.executeFdtd1d();
+    @PostMapping("/fdtd1D")
+    public void executeFDTD1D(@RequestBody Fdtd1Darguments fdtd1Darguments) throws IOException {
+        this.fdtdExecutorService.executeFdtd1d(fdtd1Darguments);
     }
 
-    @GetMapping("/fdtd2D")
-    public void executeFDTD2D() throws IOException {
-        this.fdtdExecutorService.executeFdtd1d();
+    @PostMapping("/fdtd2D")
+    public void executeFDTD2D(@RequestBody Fdtd2Darguments fdtd2Darguments) throws IOException {
+        this.fdtdExecutorService.executeFdtd2d(fdtd2Darguments);
     }
 
-    @GetMapping("/fdtd3D")
-    public void executeFDTD3D() throws IOException {
-        this.fdtdExecutorService.executeFdtd1d();
+    @PostMapping("/fdtd3D")
+    public void executeFDTD3D(@RequestBody Fdtd3Darguments fdtd3Darguments) throws IOException {
+        this.fdtdExecutorService.executeFdtd3d(fdtd3Darguments);
     }
 }
